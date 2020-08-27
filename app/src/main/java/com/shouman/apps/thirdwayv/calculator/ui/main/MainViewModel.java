862bdd204c74dee17fcdf93571f1eaa1df04ff9e
Ahmed Ramadan <ahmedshouman53@gmail.com>
2020-08-28 00:11:24 +0200

    -add the portraitLayout for the mainFragment
    -add the buttons_layout.xml
    
    -add the different actions buttons to MainViewModel.java
    
    - add Databindnig Lib and material design lib

--- app/src/main/java/com/shouman/apps/thirdwayv/calculator/ui/main/MainViewModel.java
+++ app/src/main/java/com/shouman/apps/thirdwayv/calculator/ui/main/MainViewModel.java
@@ -1,7 +1,50 @@
 package com.shouman.apps.thirdwayv.calculator.ui.main;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    public MainViewModel() {
+
+    }
+
+    public void addNumberToInput(char num) {
+
+    }
+
+    public void addZeroToInput() {
+
+    }
+
+    public void addOperator(char operator) {
+
+    }
+
+    public void clearOneByOne() {
+
+    }
+
+    public void clearAll() {
+
+    }
+
+    public void equal() {
+
+    }
+
+    public void addDot() {
+
+    }
+
+    public void undo() {
+
+    }
+
+    public void redo() {
+
+    }
+
 }
